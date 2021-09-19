7f58ceb8b801d06e008e548f9df2606581cc96b2
diiGio <83452136+diiGio@users.noreply.github.com>
2021-09-19 21:53:47 +0200

    Update OpenOrder.java

--- GestoreRistorante/src/restaurant/OpenOrder.java
+++ GestoreRistorante/src/restaurant/OpenOrder.java
@@ -84,7 +84,8 @@ public class OpenOrder extends Order implements Bidimensional, Priceable {
 		holderOrder.addOrder(new ClosedOrder(super.getOrderMap(), super.getPrice()),table);
 		// addind payment for cashier
 		this.holderPayment.addPayment(table, super.getPrice());
-		this.holderPayment.addOrder(super.getOrderMap());
+		HashMap <String, Integer> hash = super.getOrderMap();
+		this.holderPayment.addOrder(hash);
 		this.holderPayment.addMenu(menu);
 		// clearing for next order
 		this.clear();
